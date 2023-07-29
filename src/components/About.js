d787509c92baa6ef80d0a0bac200bad95ae9f47e
Priyanshu Mallick <priyanshumallick123456@gmail.com>
2023-07-29 18:34:05 +0530

    Portfolio Day 1

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,76 @@
 import React from 'react';
+// countup
+import CountUp from 'react-countup';
+// intersection observer hook
+import { useInView } from 'react-intersection-observer';
+// motion
+import { motion } from 'framer-motion';
+// varients
+import { fadeIn } from '../variants';
 
 const About = () => {
-  return <div>About</div>;
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+  return (
+    <section className='section' id='about' ref={ref}>
+      <div className="container mx-auto">
+        <div>
+          {/* img */}
+          <div className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'></div>
+          {/* text */}
+          <div>
+            <h2>
+              About Me.
+            </h2>
+            <h3>
+              I'm a Freelance Front-end Developer with over 5 years of experience.
+            </h3>
+            <p>
+              My name is Priyansshu Mallick, presently, staying in Bhubaneswar but originate from Jajpur.
+            </p>
+            {/* stats */}
+            <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {
+                    inView ?
+                      <CountUp start={0} end={15} duration={5} /> : null}
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Years of <br />
+                  Experience
+                </div>
+              </div>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {
+                    inView ?
+                      <CountUp start={0} end={13} duration={5} /> : null}
+                  k+
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Projects <br />
+                  Completed
+                </div>
+              </div>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {
+                    inView ?
+                      <CountUp start={0} end={13} duration={5} /> : null}
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Years of <br />
+                  Experience
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
