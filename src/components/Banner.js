36da7802142644549919af0dc6eec87dd6b1ef39
Priyanshu Mallick <priyanshumallick123456@gmail.com>
2023-08-04 21:11:46 +0530

    image updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // images
-import Image from '../assets/avatar.svg'
+import Image from '../assets/avatar.png'
 // icons
 import { FaDribbble, FaGit, FaGithub, FaYoutube, FarDribble } from 'react-icons/fa';
 // type animation
